2eb9e469796765492290589a3d0c9bb1c7828c11
HARSH KUMAR <connect.to.hk@gmail.com>
2022-12-16 00:58:00 +0530

    about us adjusted

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -7,11 +7,11 @@ const AboutUs = () => {
   let graphic=useScrollAnimation(['slide-left-v'],['invisible', '-translate-x-10'])
 
   return (
-    <section className="about-us-container p-10 my-24 mx-20">
+    <section className="about-us-container md:p-10 my-24 mx-10 md:mx-20">
       <h1 className="p-10 font-bold text-center text-5xl mb-2">About Us</h1>
       <div  className=" flex lg:flex-row flex-col  justify-around">
       <div className="flex gap-6 items-center">
-          <div className="text-2xl max-w-4xl invisible" ref={card}>
+          <div className="md:text-2xl text-xl md:max-w-4xl invisible" ref={card}>
               <p>One might think that the capabilities of an entrepreneurship cell is limited to gathering accolades in competitions, but here at <span className="font-bold">eCell NSUT</span>, we take it as a challenge to propel our ideas to greater heights. We take initiatives to develop and adapt ourselves into the changing popular market culture, while having a strong hold over our core and foundational values, irrespective of our predicament.</p>
               <p>eCell NSUT has been successful in creating a space for itself in the entrepreneurial circuit of India and cross borders as well. As one of the most active and cross-functional entrepreneurship entities of Delhi, our mission is to work on different projects and start-ups while becoming the most approachable eCell.</p>
           </div>
